49f490c11363962d34906558387794058e211f6f
Zeyan <zeyan.du@rea-group.com>
2023-03-12 19:50:16 +0800

    update monoid and semigroup

--- src/main/scala/catsExec/SemigroupDemo/MonoidDemo.scala
+++ src/main/scala/catsExec/SemigroupDemo/MonoidDemo.scala
@@ -4,14 +4,32 @@ import cats.Monoid
 import cats.implicits.catsSyntaxSemigroup
 
 object MonoidDemo extends App {
-  // 跟 semigroup类似，但是combine时可以定义初始值
+//  trait Semigroup[A] {
+//    def combine(x: A, y: A): A
+//  }
+//  trait Monoid[A] extends Semigroup[A] {
+//    def empty: A
+//  }
+
+  // monoid需要遵守两条规则
+  // 1. 满足结合律，如加法乘法满足，减法不满足
+  // 2. 需要定义empty(如没有empty则可以使用semigroup类型)
+
+  val fullName = "zeyan" |+| "du"
+  println(Monoid[Option[Int]].combine(Option(22), Option(10)))
 
   val intMonoid: Monoid[Int] = Monoid[Int]
   println(intMonoid.combine(2, 4))
   val zero = intMonoid.empty
 
+  // interface object
   def combineFold[T](list: List[T])(implicit monoid: Monoid[T]): T =
     list.foldLeft(monoid.empty)(_ |+| _)
+
+  // interface syntax
+  implicit class addAllOps[T](list: List[T]) {
+    def addAll(implicit monoid: Monoid[T]): T = list.foldLeft(monoid.empty)(_ |+| _)
+  }
 
   val phoneMaps: List[Map[String, Int]] = List(
     Map("zeyan1" -> 1),
@@ -20,6 +38,7 @@ object MonoidDemo extends App {
   )
 
   println(combineFold(phoneMaps))
+  println(phoneMaps.addAll)
 
   case class ShoppingCart(items: List[String], total: Double)
   implicit val shoppingCart: Monoid[ShoppingCart] = Monoid.instance(
